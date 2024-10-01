c68b52db14fa551ce123608137518258b8f21205
LeHa52 <pahtusov.lexa-52@yandex.ru>
2024-10-01 09:55:42 +0300

    Class

---
+++ Pahtusov_PR05_GIT/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pahtusov_PR05_GIT
+{
+    class Class1
+    {
+    }
+}
